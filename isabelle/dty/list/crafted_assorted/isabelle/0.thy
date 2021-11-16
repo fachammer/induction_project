bf6d5eb4ebb36bb55121d076c280876af6f76abb
Fabian Achammer <fabian@achammer.dev>
2021-11-16 12:53:33 +0100

    add induction formulas from vampire as assumptions

--- isabelle/dty/list/crafted_assorted/isabelle/0.thy
+++ isabelle/dty/list/crafted_assorted/isabelle/0.thy
@@ -13,15 +13,26 @@ fun app :: "lst \<Rightarrow> lst \<Rightarrow> lst" where
 "app nil r = r" |
 "app (cons a l) r = cons a (app l r)"
 
-lemma add_successor [simp]: "add x (s y) = s (add x y)"
-  apply(induction x)
-  by auto
+notepad
+begin
+assume app_assoc_induction_rule:
+  "\<forall>sK1.
+    (app nil (app sK1 sK1) = app (app nil sK1) sK1
+     \<longrightarrow> (\<forall>x1.\<forall>x0. (app x1 (app sK1 sK1) = app (app x1 sK1) sK1
+        \<longrightarrow> app (cons x0 x1) (app sK1 sK1) = app (app (cons x0 x1) sK1) sK1) )
+     \<longrightarrow> (\<forall>x2. app x2 (app sK1 sK1) = app (app x2 sK1) sK1))"
 
-lemma app_assoc [simp]: "app x (app y z) = app (app x y) z"
-  apply(induction x)
-  by auto
+assume add_s_induction_rule:
+  "\<forall>sK0.\<forall>sK1.
+    (add zero (s sK0) = s (add zero sK0)
+     \<longrightarrow> (\<forall>x0. add x0 (s sK0) = s (add x0 sK0)
+        \<longrightarrow> add (s x0) (s sK0) = s (add (s x0) sK0))
+     \<longrightarrow> (\<forall>x1. add x1 (s sK0) = s (add x1 sK0)))"
 
-lemma "app (cons (add n (s n)) x) (app x x) = app (app (cons (add (s n) n) x) x) x"
-  by auto
+have "\<forall>n. \<forall>x. app (cons (add n (s n)) x) (app x x) = app (app (cons (add (s n) n) x) x) x"
+using app_assoc_induction_rule add_s_induction_rule
+by simp
+
+end
 
 end
