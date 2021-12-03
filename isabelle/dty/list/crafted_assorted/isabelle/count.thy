15bf181476a832770cc1f5c6249d360c9e42f46a
Fabian Achammer <fabian@achammer.dev>
2021-12-03 12:35:47 +0100

    add leq_fn definition and proof

--- isabelle/dty/list/crafted_assorted/isabelle/count.thy
+++ isabelle/dty/list/crafted_assorted/isabelle/count.thy
@@ -9,6 +9,11 @@ inductive leq :: "nat \<Rightarrow> nat \<Rightarrow> bool" where
 zero: "leq zero n" |
 step: "leq n m \<Longrightarrow> leq (s n) (s m)"
 
+fun leq_fn :: "nat \<Rightarrow> nat \<Rightarrow> bool" where
+"leq_fn zero n = True" |
+"leq_fn (s m) zero = False" |
+"leq_fn (s m) (s n) = leq_fn m n"
+
 declare leq.intros[simp,intro]
 
 fun length :: "lst \<Rightarrow> nat" where
@@ -31,14 +36,38 @@ then show ?case by (metis nat.inject leq.simps)
 qed
 qed
 
+lemma leq_fn_s_right: "\<And>n. \<And>m. leq_fn n m \<Longrightarrow> leq_fn n (s m)"
+proof -
+fix n
+show "\<And>m. leq_fn n m \<Longrightarrow> leq_fn n (s m)" proof(induct n)
+case zero
+then show ?case by simp
+next
+case (s n)
+then show ?case by (metis leq_fn.elims(2) leq_fn.simps(1) leq_fn.simps(3))
+qed
+qed
+
+
 theorem "\<And>xs. \<And>x. leq (count x xs) (length xs)" proof -
 fix xs
 show "\<And>x. leq (count x xs) (length xs)" proof(induct xs)
 case nil
 then show ?case by simp
 next
-case (cons x1 xs)
+case (cons y ys)
 then show ?case by (simp add: leq_s_right)
+qed
+qed
+
+theorem "\<And>xs. \<And>x. leq_fn (count x xs) (length xs)" proof -
+fix xs
+show "\<And>x. leq_fn (count x xs) (length xs)" proof(induct xs)
+case nil
+then show ?case by simp
+next
+case (cons y ys)
+then show ?case by (simp add: leq_fn_s_right)
 qed
 qed
 
